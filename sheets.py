86ce01f11223aef6cd760a6c928ab2f90913c2d0
Airyy12 <143952864+Airyy12@users.noreply.github.com>
2025-07-19 23:16:38 +0800

    Update sheets.py

--- sheets.py
+++ sheets.py
@@ -1,1 +1,10 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 
+def connect_gsheet():
+    scope = ["https://spreadsheets.google.com/feeds", 
+             "https://www.googleapis.com/auth/drive"]
+    creds = ServiceAccountCredentials.from_json_keyfile_name("creds.json", scope)
+    client = gspread.authorize(creds)
+    sheet = client.open("kost-db")  # Nama Google Sheet Anda
+    return sheet
