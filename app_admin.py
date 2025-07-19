2ec8b11961c23fb518c6da623c305efd18c79bd6
Airyy12 <143952864+Airyy12@users.noreply.github.com>
2025-07-20 00:58:36 +0800

    Update app_admin.py

--- app_admin.py
+++ app_admin.py
@@ -1,12 +1,18 @@
 import streamlit as st
-from sheets import get_bookings, get_data
+from sheets import connect_gsheet
 
-st.title("Dashboard Admin Kost")
+st.title("📊 Dashboard Admin & Penyewa")
 
-if st.text_input("Masukkan password admin", type="password") == "admin123":
-    st.success("Login berhasil")
-    bookings = get_bookings()
-    st.write("Data Booking:")
-    st.dataframe(bookings)
-else:
-    st.warning("Masukkan password untuk akses dashboard")
+sheet = connect_gsheet()
+kamar_ws = sheet.worksheet("Kamar")
+booking_ws = sheet.worksheet("Booking")
+
+st.subheader("Data Booking")
+data = booking_ws.get_all_records()
+for item in data:
+    st.write(item)
+
+st.subheader("Data Kamar")
+kamar_data = kamar_ws.get_all_records()
+for k in kamar_data:
+    st.write(k)
