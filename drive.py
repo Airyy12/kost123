21131fb83619ec83e0773d7ee01126710d94628b
Airyy12 <143952864+Airyy12@users.noreply.github.com>
2025-07-20 00:57:44 +0800

    Update drive.py

--- drive.py
+++ drive.py
@@ -1,12 +1,26 @@
-from pydrive.auth import GoogleAuth
-from pydrive.drive import GoogleDrive
 import streamlit as st
+from google.oauth2 import service_account
+from googleapiclient.discovery import build
+from googleapiclient.http import MediaIoBaseUpload
+import io
 
-def upload_file(file, folder_id):
-    gauth = GoogleAuth()
-    gauth.LoadCredentialsFile("mycreds.txt")  # atau via service account
-    drive = GoogleDrive(gauth)
-    f = drive.CreateFile({'parents': [{'id': folder_id}]})
-    f.SetContentFile(file.name)
-    f.Upload()
-    return f['alternateLink']
+def upload_to_drive(file, filename, mimetype="image/jpeg"):
+    creds = service_account.Credentials.from_service_account_info(
+        st.secrets["gcp_service_account"],
+        scopes=["https://www.googleapis.com/auth/drive"]
+    )
+    service = build("drive", "v3", credentials=creds)
+
+    media = MediaIoBaseUpload(io.BytesIO(file.read()), mimetype=mimetype)
+    file_metadata = {
+        "name": filename,
+        "parents": [st.secrets["gdrive_folder_id"]],
+    }
+
+    uploaded = service.files().create(
+        body=file_metadata,
+        media_body=media,
+        fields="id"
+    ).execute()
+
+    return f"https://drive.google.com/uc?id={uploaded['id']}"
