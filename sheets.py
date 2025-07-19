9e426f6666eb721fa5b9fb17f9df42e1df4ea79c
Airyy12 <143952864+Airyy12@users.noreply.github.com>
2025-07-19 23:24:08 +0800

    Update sheets.py

--- sheets.py
+++ sheets.py
@@ -1,10 +1,23 @@
+import streamlit as st
 import gspread
 from oauth2client.service_account import ServiceAccountCredentials
+import json
+import tempfile
 
 def connect_gsheet():
-    scope = ["https://spreadsheets.google.com/feeds", 
+    scope = ["https://spreadsheets.google.com/feeds",
              "https://www.googleapis.com/auth/drive"]
-    creds = ServiceAccountCredentials.from_json_keyfile_name("creds.json", scope)
+    
+    # Ambil data dari st.secrets
+    creds_dict = json.loads(st.secrets["gcp_service_account"])
+    
+    # Simpan sementara ke file .json agar bisa dibaca oleh Google API
+    with tempfile.NamedTemporaryFile(delete=False, suffix=".json") as tmpfile:
+        json.dump(creds_dict, tmpfile)
+        tmpfile.flush()
+        creds = ServiceAccountCredentials.from_json_keyfile_name(tmpfile.name, scope)
+    
+    # Autentikasi dan buka spreadsheet
     client = gspread.authorize(creds)
-    sheet = client.open("kost-db")  # Nama Google Sheet Anda
+    sheet = client.open("kost-db")
     return sheet
