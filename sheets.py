195ba2aea4598c76f183fafd7fb13bcae0e2aa63
Airyy12 <143952864+Airyy12@users.noreply.github.com>
2025-07-20 00:57:24 +0800

    Update sheets.py

--- sheets.py
+++ sheets.py
@@ -1,25 +1,13 @@
+import gspread
 import streamlit as st
-from pydrive.auth import GoogleAuth
-from pydrive.drive import GoogleDrive
 from oauth2client.service_account import ServiceAccountCredentials
-import json
 
-def get_drive():
-    credentials_dict = json.loads(st.secrets["gcp_service_account"])
-    scope = ["https://www.googleapis.com/auth/drive"]
-    creds = ServiceAccountCredentials.from_json_keyfile_dict(credentials_dict, scope)
-    
-    gauth = GoogleAuth()
-    gauth.credentials = creds
-    return GoogleDrive(gauth)
-
-def upload_file_to_drive(file, filename):
-    drive = get_drive()
-    folder_id = st.secrets["gdrive_folder_id"]
-    gfile = drive.CreateFile({
-        'parents': [{'id': folder_id}],
-        'title': filename
-    })
-    gfile.SetContentFile(file)
-    gfile.Upload()
-    return gfile['id']
+def connect_gsheet():
+    scope = ["https://spreadsheets.google.com/feeds", "https://www.googleapis.com/auth/drive"]
+    creds = ServiceAccountCredentials.from_json_keyfile_dict(
+        st.secrets["gcp_service_account"],
+        scope
+    )
+    client = gspread.authorize(creds)
+    sheet = client.open_by_key(st.secrets["gsheet_id"])
+    return sheet
